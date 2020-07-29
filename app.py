11642aafc6d1857ea425bf896fdfd234645d1ebe
jack-hug <46361381@qq.com>
2020-07-29 16:37:49 +0800

    new0729

--- app.py
+++ app.py
@@ -8,6 +8,30 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('index.html')
 
-@app.route('/contact')
+@app.route('/Contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/Company')
+def company():
+    return render_template('company.html')
+
+@app.route('/Mold_base')
+def mold_base():
+    return render_template('mold_base.html')
+
+@app.route('/Customized')
+def customized():
+    return render_template('customized.html')
+
+@app.route('/Injection_mold')
+def injection_mold():
+    return render_template('injection_mold.html')
+
+@app.route('/Stamping_die')
+def stamping_die():
+    return render_template('stamping_die.html')
+
+@app.route('/Hot_runner')
+def hot_runner():
+    return render_template('hot_runner.html')
